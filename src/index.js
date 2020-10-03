488c159d5c8b21146e0fa8afabe6d638babcda42
Anton Kizerev <70804175+kosmodromm@users.noreply.github.com>
2020-10-03 19:20:36 +0300

    feat: complete all tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        array.sort((a, b) => a - b);
+        return array.shift()
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        array.sort((a, b) => a - b);
+        return array.pop();
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        const result = array.reduce((a, b) => a + b);
+        return result / array.length;
+    } else {
+        return 0;
+    }
 }
